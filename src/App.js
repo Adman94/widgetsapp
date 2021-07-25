169c60d7eac8948f7d4aad582b8cb260f39197cb
Adman94 <adman00794@gmail.com>
2021-07-25 15:31:10 -0700

    latest commit: wiki search api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import Accordion from './components/Accordion';
+// import Accordion from './components/Accordion';
+import Search from './components/Search';
 
 const items = [
   {
@@ -19,7 +20,8 @@ function App() {
   return (
     <div>
       <br />
-      <Accordion items={items} />
+      {/* <Accordion items={items} /> */}
+      <Search />
     </div>
   );
 }
